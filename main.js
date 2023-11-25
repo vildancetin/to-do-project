8e5308625ff37fc2ce762cb44a25a20b89e22653
Vildan Beyza Çetin <vcetin1356@gmail.com>
2023-11-25 20:12:19 +0300

    add active class to buttons

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import "./style.css";
 import "./node_modules/bootstrap/dist/js/bootstrap.bundle";
 
-
 const toDoForm = document.querySelector("#toDoForm");
 const inputForm = document.getElementById("form2");
 const buttons = document.getElementById("ex1");
@@ -9,10 +8,10 @@ const tabContent = document.querySelector(".tab-content ul");
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const createToDo = ()=>{
+const createToDo = () => {
   const inputToDo = document.getElementById("form2").value.trim();
 
-  if(inputToDo){
+  if (inputToDo) {
     const toDoObject = {
       id: new Date().getTime(),
       task: inputToDo,
@@ -21,50 +20,78 @@ const createToDo = ()=>{
     tasks.push(toDoObject);
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
-}
-toDoForm.addEventListener("submit",(e)=>{
-  e.preventDefault()
-  createToDo()
-  toDoForm.reset()
-})
-buttons.addEventListener("click",(e)=>{
-  tabContent.textContent=""
-  let filteredTodos
-  if(e.target.id==="ex1-tab-1"){
-    filteredTodos=tasks
-    showToDo(filteredTodos)
-    const checkbox = document.querySelectorAll(".check")
-    checkbox.forEach(check=>{
-      check.addEventListener("change",(e)=>{
-     check.checked= e.target.checked
-    })})
-  }else if(e.target.id==="ex1-tab-2"){
-    filteredTodos=tasks.filter(todo=>!todo.checked)
-    showToDo(filteredTodos)
-  }else if(e.target.id==="ex1-tab-3"){
-    filteredTodos=tasks.filter(todo=>todo.checked===true)
-    showToDo(filteredTodos)
-  }
-})
-
-function showToDo(todos){
-todos.forEach(todo => {
-  const li = document.createElement("li")
-  li.classList.add("list-group-item", "d-flex", "align-items-center", "border-0", "mb-2", "rounded")
-  li.style.backgroundColor="#f4f6f7"
-  li.id=todo.id
-
-  const inputCheck= document.createElement("input")
-  inputCheck.type="checkbox"
-  inputCheck.className="form-check-input me-2 check"
-  inputCheck.id=todo.id
-  inputCheck.checked=todo.checked
-
-  const label= document.createElement("label")
-  label.textContent=todo.task
-
-  li.append(inputCheck,label)
-  tabContent.append(li)
+};
+toDoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createToDo();
+  toDoForm.reset();
 });
-} 
+buttons.addEventListener("click", (e) => {
+  tabContent.textContent = "";
+  let filteredTodos;
+  if (e.target.id === "ex1-tab-1") {
+    document.querySelectorAll('a').forEach(function(link) {
+      link.classList.remove("active","bg-primary");
+    });
+    e.target.classList.add("active","bg-primary")
+    filteredTodos = tasks;
+    showToDo(filteredTodos);
+  } else if (e.target.id === "ex1-tab-2") {
+    document.querySelectorAll('a').forEach(function(link) {
+      link.classList.remove("active","bg-primary");
+    });
+    e.target.classList.add("active","bg-primary")
+    filteredTodos = tasks.filter((todo) => !todo.checked);
+    showToDo(filteredTodos);
+  } else if (e.target.id === "ex1-tab-3") {
+    document.querySelectorAll('a').forEach(function(link) {
+      link.classList.remove("active","bg-primary");
+    });
+    e.target.classList.add("active","bg-primary")
+    filteredTodos = tasks.filter((todo) => todo.checked === true);
+    showToDo(filteredTodos);
+  }
+});
 
+function showToDo(todos) {
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "border-0",
+      "mb-2",
+      "rounded"
+    );
+    li.style.backgroundColor = "#f4f6f7";
+    li.id = todo.id;
+
+    const inputCheck = document.createElement("input");
+    inputCheck.type = "checkbox";
+    inputCheck.className = "form-check-input me-2 check";
+    inputCheck.id = todo.id;
+    inputCheck.checked = todo.checked;
+
+    inputCheck.addEventListener("change", () => {
+      todo.checked = inputCheck.checked;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      tabContent.innerHTML=""
+      showToDo(tasks)
+    });
+
+    const label = document.createElement("label");
+    label.textContent = todo.task;
+
+    li.append(inputCheck, label);
+    
+    if (todo.checked) {
+      li.classList.add("completed");
+    }
+    tabContent.append(li);
+  });
+}
+
+window.addEventListener("load",()=>{
+  showToDo(tasks)
+})
